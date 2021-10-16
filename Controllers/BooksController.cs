898899ade28f58393f7212057c5b840ab61d97cc
Staniszewski Łukasz <lukasz.staniszewski@microsoft.wsei.edu.pl>
2021-10-16 16:35:42 +0200

    ehh add books

---
+++ Controllers/BooksController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using lib_app_wsei.Models;
+
+namespace lib_app_wsei.Controllers
+{
+    public class BooksController : Controller
+    {
+        public IActionResult Random()
+        {
+            Book firstBook = new Book() { Name = "English dictionary"};
+            return View(firstBook);
+        }
+    }
+}
