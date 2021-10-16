c54acc75211d9daa2e6f111f09e00e4ff027fb9b
Staniszewski Łukasz <lukasz.staniszewski@microsoft.wsei.edu.pl>
2021-10-16 16:16:01 +0200

    first commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace lib_app_wsei.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
